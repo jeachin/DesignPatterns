a4ffe8846e232a04e6d35f73ee49dd4541a2d03c
caijiacheng <caijiacheng@megvii.com>
2020-09-09 15:36:18 +0800

    feat: 增加代理模式代码示例

---
+++ src/main/java/aidd/proxy/ReceiveGift.java
@@ -0,0 +1,14 @@
+package aidd.proxy;
+
+/**
+ * 抽象主题类
+ * @author caijiacheng
+ * @create 2020/9/9
+ */
+public interface ReceiveGift {
+    /**
+     * 接受礼物
+     * @param gift
+     */
+    void receive(String gift);
+}
