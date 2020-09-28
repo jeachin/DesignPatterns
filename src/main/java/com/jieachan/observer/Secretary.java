fc7b9ec651bd3d2182360ec73d5ba4624550da07
caijiacheng <caijiacheng@megvii.com>
2020-09-28 10:22:04 +0800

    feat: 增加观察者模式

---
+++ src/main/java/com/jieachan/observer/Secretary.java
@@ -0,0 +1,49 @@
+package com.jieachan.observer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 前台
+ *
+ * @author jieachan
+ * @create 2020/9/28
+ */
+public class Secretary implements Subject {
+
+    //同事列表
+    private List<Observer> observers = new LinkedList<>();
+    private String action;
+
+    //添加
+    @Override
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    //删除
+    @Override
+    public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    //通知
+    @Override
+    public void notifyObservers() {
+        for(Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    //前台状态
+    @Override
+    public String getAction() {
+        return action;
+    }
+
+    @Override
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+}
